ce8495fc7c5370734267b468d224a297cb97c84a
Dragan Babic <dragan@sprawsm.com>
2017-04-06 13:20:53 +0200

    Adding support for drawers and scrollfix.
    There is a new class `.scrollfix` which makes the body not scroll while
    the user is scrolling an element with `overflow: auto`.

--- source/js/app.js
+++ source/js/app.js
@@ -178,6 +178,37 @@ $(document).ready(function() {
   });
 
   // ===========================================================================
+  // Make scrolling comfortable
+  // 
+  // http://stackoverflow.com/a/15821417
+
+  $.fn.scrollfix = function() {
+      this.bind('mousewheel DOMMouseScroll', function (e) {
+          var delta = e.wheelDelta || (e.originalEvent && e.originalEvent.wheelDelta) || -e.detail,
+              bottomOverflow = this.scrollTop + $(this).outerHeight() - this.scrollHeight >= 0,
+              topOverflow = this.scrollTop <= 0;
+
+          if ((delta < 0 && bottomOverflow) || (delta > 0 && topOverflow)) {
+              e.preventDefault();
+          }
+      });
+      return this;
+  };
+
+  $('.scrollfix').scrollfix();
+
+  // ===========================================================================
+  // Drawers
+
+  var drawerToggle = $('[data-toggle="drawer"]');
+  var drawer = $('.drawer');
+
+  drawerToggle.click(function(e) {
+    drawer.filter(this.hash).toggleClass('in');
+    e.preventDefault();
+  });
+
+  // ===========================================================================
   // Bootstrap tabs update
 
   var tabHashPrefix = 'tab-';
